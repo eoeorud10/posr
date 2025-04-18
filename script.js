8c949bfd512b1c23cd65c0ac4166659d4f8d828f
eoeorud10 <eoeorud10@gmail.com>
2025-04-18 23:40:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -29,20 +29,7 @@
         menu.classList.toggle("open");
     }
 
-/*
-//모달
-    function openModal(element) {
-      const modal = document.getElementById("imageModal");
-      const modalImg = document.getElementById("modalImage");
-      const imgSrc = element.querySelector("img").src;
-      modal.style.display = "block";
-      modalImg.src = imgSrc;
-    }
 
-    function closeModal() {
-      document.getElementById("imageModal").style.display = "none";
-    }
-*/
 
 
 
